56b40889ff47b8643c0af65db4b815d8943b5858
Leonardo Nunes Carvalho <worldrulesdev@gmail.com>
2022-10-17 17:04:27 -0300

    google fonts added

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Blog Codar</title>
+  <link rel="stylesheet" href="css/styles.css">
+  <!-- GOOGLE FONTS -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+</body>
+
+</html>
