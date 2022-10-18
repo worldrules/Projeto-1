b02ba2eb68bfb07fb4c204a696a2c18e026c5dba
Leonardo Nunes Carvalho <worldrulesdev@gmail.com>
2022-10-18 17:43:35 -0300

    kek

--- templates/header.php
+++ templates/header.php
@@ -1,19 +1,31 @@
+<?php
+include_once ("data/categories.php");
+include_once ("data/posts.php");
+include_once ("helpers/url.php");
+?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Blog Codar</title>
-  <link rel="stylesheet" href="css/styles.css">
+  <!-- ESTILOS DO PROJETO -->
+  <link rel="stylesheet" href="./css/styles.css">
   <!-- GOOGLE FONTS -->
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
 </head>
-
 <body>
   <header>
-    Template Cabeçalho
+    <a href="." id="logo">
+      <img src="./img/logo.svg" alt="Blog Codar">
+    </a>
+    <nav>
+      <ul id="navbar">
+        <li><a href="." class="nav-link">Home</a></li>
+        <li><a href="#" class="nav-link">Categorias</a></li>
+        <li><a href="#" class="nav-link">Sobre</a></li>
+        <li><a href="./contato.php" class="nav-link">Contato</a></li>
+      </ul>
+    </nav>
   </header>
-</body>
